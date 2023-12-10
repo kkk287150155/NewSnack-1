ea693150ccf560fc2a157e33a31533cabf3b3586
Kim <kkk287150155@naver.com>
2023-12-10 14:11:12 +0900

    Co-authored-by: Kimhhhhhh <Kimhhhhhh@users.noreply.github.com>

--- src/InitialSetup/index.tsx
+++ src/InitialSetup/index.tsx
@@ -9,7 +9,6 @@ import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Slider from "@mui/material/Slider";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 import store from "../localStorage";
 
